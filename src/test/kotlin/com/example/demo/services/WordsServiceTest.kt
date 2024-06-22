899643558efd55b2251ae44b6436cf2fb0c0522a
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2024-06-22 09:32:25 +0200

    add test for WordsService

--- src/test/kotlin/com/example/demo/services/WordsServiceTest.kt
+++ src/test/kotlin/com/example/demo/services/WordsServiceTest.kt
@@ -2,17 +2,25 @@ package com.example.demo.services
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.web.server.ResponseStatusException
 
 @SpringBootTest
 class WordsServiceTest {
-    private val requiredNumberOfWords = 5
     private val wordsService = WordsService()
 
     @Test
     fun `should return five random words`() {
-        val result = wordsService.getRandomWords(requiredNumberOfWords)
+        val numberOfWords = 5
+        val result = wordsService.getRandomWords(numberOfWords)
 
         assertThat(result).size().isEqualTo(5)
+    }
+
+    @Test
+    fun `should throw error if too many words are requested`() {
+        val numberOfWords = 6000
+        assertThrows<ResponseStatusException> { wordsService.getRandomWords(numberOfWords) }
     }
 }
