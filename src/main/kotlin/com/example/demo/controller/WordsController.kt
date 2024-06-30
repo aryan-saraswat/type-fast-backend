e75f2188f620e661d11076189676450de9b4ca0d
Aryan Saraswat <aryan.saraswat@hotmail.co.uk>
2024-06-30 23:15:51 +0200

    add react and angular port values to crossorigin

--- src/main/kotlin/com/example/demo/controller/WordsController.kt
+++ src/main/kotlin/com/example/demo/controller/WordsController.kt
@@ -9,10 +9,16 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class WordsController(val wordsService: WordsService) {
-    @CrossOrigin(origins = ["http://localhost:4200"])
+
+    @CrossOrigin(origins = ["http://localhost:${reactPort}", "http://localhost:${angularPort}"])
     @PostMapping("/get-words")
     fun randomWords(@RequestBody wordsRequest: WordsRequest): List<String> {
         println("randomWords function called")
         return wordsService.getRandomWords(wordsRequest.numberOfWords)
+    }
+
+    companion object {
+        const val angularPort = 4200
+        const val reactPort = 3000
     }
 }
